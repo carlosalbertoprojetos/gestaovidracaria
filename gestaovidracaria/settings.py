5a0a298b6ddc715bbbe279699c185a2021945eba
Flaviobh <flavio.piunibh@gmail.com>
2022-08-09 01:20:37 -0300

    atualizacao settings

--- gestaovidracaria/settings.py
+++ gestaovidracaria/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,4 +141,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
