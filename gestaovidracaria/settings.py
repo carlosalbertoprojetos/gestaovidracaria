7d306b5aaa69611ad2319f10529bec00997167d5
Flaviobh <flavio.piunibh@gmail.com>
2022-08-09 11:13:57 -0300

    atualizacao settings

--- gestaovidracaria/settings.py
+++ gestaovidracaria/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import django_on_heroku
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
